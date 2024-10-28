ba7ecd04b667e86da10ebabd24fc50476fbb8a68
artias13 <archee.busy@gmail.com>
2024-10-28 13:59:09 +0800

    files

---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask, request, jsonify, Response
+from dotenv import load_dotenv
+
+load_dotenv()
+HOST_URL = os.getenv('HOST_URL')
+
+class Server:
+    def __init__(self):
+        self.app = Flask(__name__)
+        self.setup_routes()
+        
+    def setup_routes(self):
+        @self.app.route('/', methods=['GET', 'OPTIONS'])
+        def home(self):
+            reply = "Try /healthz bro"
+
+        @self.app.route('/healthz', methods=['GET', 'OPTIONS'])
+        def healthz(self):
+            return Response("OK", status=200)
+
+    def run(self, debug, port, host):
+        print(f'ну давай, нападай: http://{host}:{port}')
+        self.app.run(debug=debug, port=port, host=host)
+
+if __name__ == '__main__':
+    server = Server()
+    server.run(debug=True, port=8225, host=str(HOST_URL))
